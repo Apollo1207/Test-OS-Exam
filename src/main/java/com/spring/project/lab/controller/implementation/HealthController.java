27fc3625bf74cc0db6e7b31332a323f7edfefc8b
Apollo1207 <fednyakillya12072001@gmail.com>
2021-05-30 16:15:42 +0300

    added OS Lab

---
+++ src/main/java/com/spring/project/lab/controller/implementation/HealthController.java
@@ -0,0 +1,13 @@
+package com.spring.project.lab.controller.implementation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HealthController {
+    @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
+    public ResponseEntity returnHealthCheckResponse() {
+        return new ResponseEntity<>("Everything is OK", HttpStatus.OK);
+    }
+}
